ef2a06dce3b98079a7cc2dfb9f3f296bc0b61148
Mohamed Imad <email2emad@gmail.com>
2025-07-17 20:56:24 +0500

    debug

--- App/Views/gallery.view.php
+++ App/Views/gallery.view.php
@@ -30,90 +30,73 @@
 </section>
 
 <section class="text-gray-400 bg-gray-900 body-font">
-    <div class="container px-5 py-24 mx-auto flex flex-wrap">
-        <div class="flex w-full mb-20 flex-wrap">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font text-white lg:w-1/3 lg:mb-0 mb-4">Our 3D Printing Gallery</h1>
-            <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Browse a selection of our recent 3D printed works. From engineering prototypes to creative art pieces, our gallery showcases the versatility and precision of our printing technology. Get inspired for your next project!</p>
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col text-center w-full mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Master Cleanse Reliac Heirloom</h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
         </div>
-        <div class="flex flex-wrap md:-m-2 -m-1">
-            <div class="flex flex-wrap w-full md:w-1/2">
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/500x300">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-full">
-                    <img alt="gallery" class="rounded-lg w-full h-full object-cover object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/600x360">
-                </div>
-            </div>
-            <div class="flex flex-wrap w-full md:w-1/2">
-                <div class="md:p-2 p-1 w-full md:w-full">
-                    <img alt="gallery" class="rounded-lg w-full h-full object-cover object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/600x360">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/500x300">
+        <div class="flex flex-wrap -m-4">
+            <div class="lg:w-1/3 sm:w-1/2 p-4">
+                <div class="flex relative">
+                    <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/600x360">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                        <h2 class="tracking-widest text-sm title-font font-medium text-yellow-400 mb-1">THE SUBTITLE</h2>
+                        <h1 class="title-font text-lg font-medium text-white mb-3">Shooting Stars</h1>
+                        <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    </div>
                 </div>
             </div>
-            <div class="flex flex-wrap w-full md:w-1/2">
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-full">
-                    <img alt="gallery" class="rounded-lg w-full h-full object-cover object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/600x360">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/500x300">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
+            <div class="lg:w-1/3 sm:w-1/2 p-4">
+                <div class="flex relative">
+                    <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/601x361">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                        <h2 class="tracking-widest text-sm title-font font-medium text-yellow-400 mb-1">THE SUBTITLE</h2>
+                        <h1 class="title-font text-lg font-medium text-white mb-3">The Catalyzer</h1>
+                        <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    </div>
                 </div>
             </div>
-            <div class="flex flex-wrap w-full md:w-1/2">
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/500x300">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-full">
-                    <img alt="gallery" class="rounded-lg w-full h-full object-cover object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/600x360">
-                </div>
-            </div>
-            <div class="flex flex-wrap w-full md:w-1/2">
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/500x300">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
-                </div>
-                <div class="md:p-2 p-1 w-full md:w-full">
-                    <img alt="gallery" class="rounded-lg w-full h-full object-cover object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/600x360">
+            <div class="lg:w-1/3 sm:w-1/2 p-4">
+                <div class="flex relative">
+                    <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/603x363">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                        <h2 class="tracking-widest text-sm title-font font-medium text-yellow-400 mb-1">THE SUBTITLE</h2>
+                        <h1 class="title-font text-lg font-medium text-white mb-3">The 400 Blows</h1>
+                        <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    </div>
                 </div>
             </div>
-            <div class="flex flex-wrap w-full md:w-1/2">
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
+            <div class="lg:w-1/3 sm:w-1/2 p-4">
+                <div class="flex relative">
+                    <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/602x362">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                        <h2 class="tracking-widest text-sm title-font font-medium text-yellow-400 mb-1">THE SUBTITLE</h2>
+                        <h1 class="title-font text-lg font-medium text-white mb-3">Neptune</h1>
+                        <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    </div>
                 </div>
-                <div class="md:p-2 p-1 w-full md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4">
+                <div class="flex relative">
+                    <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/605x365">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                        <h2 class="tracking-widest text-sm title-font font-medium text-yellow-400 mb-1">THE SUBTITLE</h2>
+                        <h1 class="title-font text-lg font-medium text-white mb-3">Holden Caulfield</h1>
+                        <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    </div>
                 </div>
-                <div class="md:p-2 p-1 w-full md:w-full">
-                    <img alt="gallery" class="rounded-lg w-full h-full object-cover object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/600x360">
-                </div>
-                <div class="md:p-2 p-1 md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/500x300">
-                </div>
-                <div class="md:p-2 p-1 md:w-1/2">
-                    <img alt="gallery" class="rounded-lg w-full object-cover h-full object-center block transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" src="https://dummyimage.com/501x301">
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4">
+                <div class="flex relative">
+                    <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/606x366">
+                    <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                        <h2 class="tracking-widest text-sm title-font font-medium text-yellow-400 mb-1">THE SUBTITLE</h2>
+                        <h1 class="title-font text-lg font-medium text-white mb-3">Alper Kamu</h1>
+                        <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
 </section>
 <!-- endsection gallery -->
