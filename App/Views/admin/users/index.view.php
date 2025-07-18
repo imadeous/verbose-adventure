e75f5dd87bea15de951cad57447b58ea4aab38e4
Mohamed Imad <email2emad@gmail.com>
2025-07-18 19:23:46 +0500

    added Models

--- App/Views/admin/users/index.view.php
+++ App/Views/admin/users/index.view.php
@@ -39,12 +39,6 @@
                             <td class="px-6 py-4 whitespace-nowrap text-gray-700"><?= e($user->email) ?></td>
                             <td class="px-6 py-4 whitespace-nowrap text-gray-700"><?= e($user->role) ?></td>
                             <td class="px-6 py-4 whitespace-nowrap flex items-center space-x-2">
-                                <a href="<?= url('admin/users/' . $user->id) ?>" class="text-blue-600 hover:underline">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                    </svg>
-                                </a>
                                 <?php if (Auth::isAdmin()): ?>
                                     <a href="<?= url('admin/users/' . $user->id . '/edit') ?>" class="text-yellow-600 hover:underline">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
