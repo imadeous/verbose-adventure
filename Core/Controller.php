403dd57e666348032ae49e877407d48500c93c3b
Mohamed Imad <email2emad@gmail.com>
2025-07-18 17:32:22 +0500

    debug

--- Core/Controller.php
+++ Core/Controller.php
@@ -5,30 +5,14 @@ namespace Core;
 class Controller
 {
     protected $view;
-    protected $breadcrumb = [];
 
     public function __construct()
     {
         $this->view = new \Core\View();
     }
 
-    /**
-     * Set the breadcrumb array for the view.
-     * @param array $breadcrumb
-     */
-    protected function breadcrumb(array $breadcrumb)
-    {
-        $this->breadcrumb = $breadcrumb;
-    }
-
-    /**
-     * Render a view, automatically passing breadcrumb if set.
-     */
     protected function view($name, $data = [])
     {
-        if (!isset($data['breadcrumb']) && !empty($this->breadcrumb)) {
-            $data['breadcrumb'] = $this->breadcrumb;
-        }
         echo $this->view->render($name, $data);
     }
 }
