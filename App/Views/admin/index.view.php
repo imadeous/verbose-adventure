d2eb57fd50fd5a1e381414894dec2dca1e66cf14
Mohamed Imad <email2emad@gmail.com>
2025-07-19 09:23:08 +0500

    added Models

--- App/Views/admin/index.view.php
+++ App/Views/admin/index.view.php
@@ -183,53 +183,59 @@
                 Recent Reviews
             </h4>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                <div class="flex flex-col gap-2 p-4 rounded-lg bg-blue-50 border border-blue-100">
-                    <div>
-                        <span class="font-semibold text-blue-900">Aishath L.</span>
-                        <span class="block text-xs text-blue-400">Custom Keychain</span>
-                    </div>
-                    <div class="flex items-center gap-3 mt-2">
-                        <div class="flex items-center gap-1">
-                            <span class="text-yellow-400">★</span><span>5</span>
-                        </div>
-                        <div class="flex items-center gap-1">
-                            <span class="text-blue-500">💲</span><span>4</span>
-                        </div>
-                        <div class="flex items-center gap-1">
-                            <span class="text-blue-400">🚚</span><span>5</span>
-                        </div>
-                    </div>
-                    <div class="text-blue-700 italic text-xs mt-2">"Great quality and fast delivery!"</div>
-                </div>
-                <div class="flex flex-col gap-2 p-4 rounded-lg bg-blue-50 border border-blue-100">
-                    <div>
-                        <span class="font-semibold text-blue-900">Mohamed R.</span>
-                        <span class="block text-xs text-blue-400">Miniature Figure</span>
-                    </div>
-                    <div class="flex items-center gap-3 mt-2">
-                        <div class="flex items-center gap-1">
-                            <span class="text-yellow-400">★</span><span>4</span>
-                        </div>
-                        <div class="flex items-center gap-1">
-                            <span class="text-blue-500">💲</span><span>5</span>
-                        </div>
-                        <div class="flex items-center gap-1">
-                            <span class="text-blue-400">🚚</span><span>4</span>
-                        </div>
-                    </div>
-                    <div class="text-blue-700 italic text-xs mt-2">"Very detailed and well-priced."</div>
-                </div>
+
                 <div class="flex flex-col gap-2 p-4 rounded-lg bg-blue-50 border border-blue-100">
                     <div class="flex justify-between items-center">
                         <div>
                             <span class="font-semibold text-blue-900">Fathimath S.</span>
                             <span class="block text-xs text-blue-400">Architectural Model</span>
                         </div>
+                        <!-- Product Quality -->
                         <div class="flex items-center gap-3 mt-2">
                             <div class="flex items-center gap-1">
                                 <span class="text-yellow-400">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                    </svg>
+                                </span><span>5</span>
+                            </div>
+                        </div>
+                        <!-- Pricing -->
+                        <div class="flex items-center gap-3 mt-2">
+                            <div class="flex items-center gap-1">
+                                <span class="text-yellow-400">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                                    </svg>
+                                </span><span>5</span>
+                            </div>
+                        </div>
+                        <!-- Communication -->
+                        <div class="flex items-center gap-3 mt-2">
+                            <div class="flex items-center gap-1">
+                                <span class="text-yellow-400">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+                                    </svg>
+                                </span><span>5</span>
+                            </div>
+                        </div>
+                        <!-- Packaging -->
+                        <div class="flex items-center gap-3 mt-2">
+                            <div class="flex items-center gap-1">
+                                <span class="text-yellow-400">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                                    </svg>
+                                </span><span>5</span>
+                            </div>
+                        </div>
+                        <!-- Delivery -->
+                        <div class="flex items-center gap-3 mt-2">
+                            <div class="flex items-center gap-1">
+                                <span class="text-yellow-400">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
                                     </svg>
                                 </span><span>5</span>
                             </div>
