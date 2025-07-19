dbc13848e58e73b3252f786accdc69fa34b3217b
Mohamed Imad <email2emad@gmail.com>
2025-07-19 18:41:41 +0500

    debug

--- App/Views/admin/contacts/index.view.php
+++ App/Views/admin/contacts/index.view.php
@@ -33,7 +33,7 @@
                                 <?= htmlspecialchars(mb_strimwidth($contact->message, 0, 50, '...')) ?>
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap text-gray-700<?= $contact->opened_at === null ? ' font-bold' : '' ?>"><?= htmlspecialchars($contact->created_at) ?></td>
-                            <td class="px-6 py-4 whitespace-nowrap flex items-end space-x-2">
+                            <td class="px-6 py-4 whitespace-nowrap flex space-x-2">
                                 <a href="<?= url('admin/contacts/' . $contact->id) ?>" class="text-yellow-600 hover:underline" title="View">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
