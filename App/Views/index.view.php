573ead191849f40d7c3a7b3333a52b7d859403c5
Mohamed Imad <email2emad@gmail.com>
2025-07-19 16:49:11 +0500

    added Models

--- App/Views/index.view.php
+++ App/Views/index.view.php
@@ -95,7 +95,7 @@
     <!--featured section-->
     <section class="text-gray-400 bg-gray-900 py-12 body-font">
         <div class="container px-5 py-24 mx-auto flex flex-wrap">
-            <div class="flex w-full mb-20 flex-wrap">
+            <div class="flex flex-col w-full mb-20 flex-wrap">
                 <h1 class="sm:text-3xl text-2xl font-medium title-font text-white lg:w-1/3 lg:mb-0 mb-4">Featured Prints & Projects</h1>
                 <div class="h-1 w-20 bg-yellow-500 rounded"></div>
                 <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Explore our latest 3D printed creations, from custom prototypes to artistic models. Each project showcases the versatility and quality of our printing technology. Get inspired for your next idea!</p>
