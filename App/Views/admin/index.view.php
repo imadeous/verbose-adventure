f83b8ecab9b2741fdf33ef9e62ef42da8bbc0182
Mohamed Imad <email2emad@gmail.com>
2025-07-19 09:18:18 +0500

    added Models

--- App/Views/admin/index.view.php
+++ App/Views/admin/index.view.php
@@ -227,13 +227,11 @@
                         </div>
                         <div class="flex items-center gap-3 mt-2">
                             <div class="flex items-center gap-1">
-                                <span class="text-yellow-400">★</span><span>5</span>
-                            </div>
-                            <div class="flex items-center gap-1">
-                                <span class="text-blue-500">💲</span><span>5</span>
-                            </div>
-                            <div class="flex items-center gap-1">
-                                <span class="text-blue-400">🚚</span><span>5</span>
+                                <span class="text-yellow-400">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                    </svg>
+                                </span><span>5</span>
                             </div>
                         </div>
                     </div>
