ed76875a3b3b93f29bbc0e47be3c1cd9b027f807
Mohamed Imad <email2emad@gmail.com>
2025-07-19 10:27:20 +0500

    added Models

--- App/Views/layouts/admin.php
+++ App/Views/layouts/admin.php
@@ -24,10 +24,10 @@
             <?php echo $this->partial('admin/header'); ?>
             <!-- add breadcrumbs support here   -->
             <div class="flex-1 overflow-y-auto">
-                <?php echo $this->yield('content'); ?>
                 <?php echo $this->partial('_flash.view'); ?>
                 <div class="p-6 lg:p-12 space-y-8 flex-1">
-                    <?= $content ?>
+                    <?php echo $this->yield('content'); ?>
+                    <!-- <?= $content ?> -->
                 </div>
                 <?php echo $this->partial('admin/footer'); ?>
         </main>
