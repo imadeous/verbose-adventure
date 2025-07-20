eec6615d913d71b598b33b8e22018de4fadc4562
Mohamed Imad <email2emad@gmail.com>
2025-07-20 13:19:17 +0500

    debug

--- config/schema.sql
+++ config/schema.sql
@@ -50,6 +50,7 @@ CREATE TABLE products (
     name VARCHAR(100) NOT NULL,
     category_id INT,
     description TEXT,
+    price DECIMAL(10,2) NOT NULL,
     image_url VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
