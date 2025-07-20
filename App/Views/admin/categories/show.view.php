c0866d0ce33b2f867c1b6f4c6e6700ba1d4f041f
Mohamed Imad <email2emad@gmail.com>
2025-07-20 14:05:33 +0500

    debug

--- App/Views/admin/categories/show.view.php
+++ App/Views/admin/categories/show.view.php
@@ -18,6 +18,7 @@
     <a href="<?= url('admin/categories/' . $category->id . '/edit') ?>" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">Edit</a>
     <a href="<?= url('admin/categories') ?>" class="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition">Back to List</a>
 </div>
+<?php var_dump($products) ?>
 <?php
 foreach ($products as $product): ?>
     <div class="bg-white shadow rounded-lg p-4 mb-4">
