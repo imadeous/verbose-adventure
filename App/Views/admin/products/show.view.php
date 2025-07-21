59d6c6b0a42200bd5da624e55aaf3e393474458a
Mohamed Imad <email2emad@gmail.com>
2025-07-21 05:29:18 +0500

    debug

--- App/Views/admin/products/show.view.php
+++ App/Views/admin/products/show.view.php
@@ -2,7 +2,7 @@
     <!-- Top Section: 2 Cards -->
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
         <!-- Card 1: Product Details -->
-        <div class="col-span-2 bg-blue-50 rounded-xl shadow-md p-5 flex flex-col gap-4 border border-blue-200 hover:shadow-lg transition">
+        <div class="col-span-2 bg-blue-50 rounded-xl shadow-md flex flex-col gap-4 border border-blue-200 hover:shadow-lg transition">
             <div class="flex items-center gap-4 mb-2">
                 <div class="bg-blue-200 text-blue-700 rounded-lg p-3 flex items-center justify-center">
                     <!-- Product icon -->
