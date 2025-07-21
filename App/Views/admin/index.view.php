2e5222ec0f68a2b56e55655ad806b9552df0ef60
Mohamed Imad <email2emad@gmail.com>
2025-07-21 18:28:39 +0500

    debug

--- App/Views/admin/index.view.php
+++ App/Views/admin/index.view.php
@@ -318,8 +318,8 @@
                             <span class="font-medium text-blue-900 flex-1"><?php echo htmlspecialchars($product['name']); ?></span>
                             <div class="flex items-center gap-1 bg-blue-200 px-2 py-1 rounded-full">
                                 <span class="text-blue-700 font-bold"><?php echo number_format($product['rating'], 1); ?></span>
-                                <svg class="w-4 h-4 text-blue-700" fill="currentColor" viewBox="0 0 20 20">
-                                    <polygon points="10 15 4 18 5.5 12 1 8 7 7 10 1 13 7 19 8 14.5 12 16 18"></polygon>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 text-blue-700">
+                                    <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
                                 </svg>
                             </div>
                         </div>
