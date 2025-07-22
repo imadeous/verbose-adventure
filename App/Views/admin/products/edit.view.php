55883b6b4fbc03534843fcd33265c65379da23a0
Mohamed Imad <email2emad@gmail.com>
2025-07-22 15:37:55 +0500

    debug

--- App/Views/admin/products/edit.view.php
+++ App/Views/admin/products/edit.view.php
@@ -6,7 +6,6 @@
             <input type="text" name="name" class="w-full rounded-lg border border-blue-300 focus:border-blue-500 focus:ring-blue-500 px-4 py-2 text-gray-900 bg-blue-50" value="<?= e($product->name) ?>" required>
         </div>
         <div>
-            <?php var_dump($categories); ?>
             <label class="block text-sm font-medium text-gray-700 mb-1">Category</label>
             <select name="category" class="w-full rounded-lg border border-blue-300 focus:border-blue-500 focus:ring-blue-500 px-4 py-2 text-gray-900 bg-blue-50">
                 <?php foreach ($categories as $cat): ?>
