93931df666d8fb6af723338455d864eb65fdb0c4
Mohamed Imad <email2emad@gmail.com>
2025-07-23 15:22:43 +0500

    updated Query Builder & Model

--- App/Models/Product.php
+++ App/Models/Product.php
@@ -53,7 +53,7 @@ class Product extends Model
             ->join('products', 'reviews.product_id', '=', 'products.id')
             ->where('reviews.product_id', '=', $id)
             ->get();
-        return array_map(fn($row) => new Review($row), $reviewRows);
+        return $reviewRows;
     }
 
     // return an object with overall product rating
