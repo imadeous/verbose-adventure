b0700af2a372cb2debfd1756182b3cd2d150f3ca
Mohamed Imad <email2emad@gmail.com>
2025-07-23 16:01:03 +0500

    updated Query Builder & Model

--- App/Views/admin/categories/index.view.php
+++ App/Views/admin/categories/index.view.php
@@ -22,18 +22,18 @@
                                 <div class="font-semibold text-blue-900"><?= e($category['name']) ?></div>
                             </td>
                             <td class="px-4 py-2 whitespace-nowrap flex items-center space-x-2">
-                                <a href="<?= url('admin/categories/' . $category->id) ?>" class="bg-blue-100 text-blue-700 hover:bg-blue-200 border border-blue-300 rounded px-2 py-1 flex items-center gap-1 transition shadow-sm" title="Edit">
+                                <a href="<?= url('admin/categories/' . $category['id']) ?>" class="bg-blue-100 text-blue-700 hover:bg-blue-200 border border-blue-300 rounded px-2 py-1 flex items-center gap-1 transition shadow-sm" title="Edit">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                     </svg>
                                 </a>
-                                <a href="<?= url('admin/categories/' . $category->id . '/edit') ?>" class="bg-yellow-100 text-yellow-700 hover:bg-yellow-200 border border-yellow-300 rounded px-2 py-1 flex items-center gap-1 transition shadow-sm" title="Edit">
+                                <a href="<?= url('admin/categories/' . $category['id'] . '/edit') ?>" class="bg-yellow-100 text-yellow-700 hover:bg-yellow-200 border border-yellow-300 rounded px-2 py-1 flex items-center gap-1 transition shadow-sm" title="Edit">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                     </svg>
                                 </a>
-                                <form action="<?= url('admin/categories/' . $category->id . '/delete') ?>" method="POST" style="display:inline;">
+                                <form action="<?= url('admin/categories/' . $category['id'] . '/delete') ?>" method="POST" style="display:inline;">
                                     <?= csrf_field() ?>
                                     <button type="submit" class="bg-red-100 text-red-700 hover:bg-red-200 border border-red-200 rounded px-2 py-1 flex items-center gap-1 transition shadow-sm hover:cursor-pointer" title="Delete" onclick="return confirm('Delete this user?')">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
