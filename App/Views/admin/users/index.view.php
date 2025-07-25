5318b91ddaba69d2784ad50fa393b2d04d4feaaf
Mohamed Imad <email2emad@gmail.com>
2025-07-25 17:31:59 +0500

    Added ChartBuilder

--- App/Views/admin/users/index.view.php
+++ App/Views/admin/users/index.view.php
@@ -26,7 +26,8 @@
                     <?php foreach ($users as $user): ?>
                         <tr class="border-t border-blue-100 hover:bg-blue-50 transition">
                             <td class="px-4 py-2 whitespace-nowrap">
-                                <div class="font-semibold text-blue-900"><?= e($user->username) ?>
+                                <div class="font-semibold text-blue-900 flex items-center gap-2">
+                                    <span><?= e($user->username) ?></span>
                                     <?php if (!is_null($user->verified_at)): ?>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4 text-green-500">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
