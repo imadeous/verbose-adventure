1afa46785b8ea5b5ec4ccdee268d0a6a54a5d169
Mohamed Imad <email2emad@gmail.com>
2025-07-25 22:18:52 +0500

    Added ReportBuilder

--- App/Controllers/Admin/TransactionController.php
+++ App/Controllers/Admin/TransactionController.php
@@ -24,10 +24,10 @@ class TransactionController extends AdminController
             ->forPeriod(date('Y-m-01'), date('Y-m-t')) // Aggregate for the current month
             ->monthly()
             ->withSum('amount', 'Total')
-            ->withMax('amount', 'Max')
-            ->withMin('amount', 'Min')
-            ->withAverage('amount', 'Average')
-            ->withCount('*', 'Transactions')
+            // ->withMax('amount', 'Max')
+            // ->withMin('amount', 'Min')
+            // ->withAverage('amount', 'Average')
+            // ->withCount('*', 'Transactions')
             ->generate('My Report Title');
 
         $this->view->layout('admin');
