bf3a12549be947b1eb50311c1434b5a80680b993
Mohamed Imad <email2emad@gmail.com>
2025-07-25 22:01:24 +0500

    Added ReportBuilder

--- App/Views/admin/transactions/index.view.php
+++ App/Views/admin/transactions/index.view.php
@@ -50,14 +50,25 @@
             </tr>
         </thead>
         <tbody>
-            <?php if (empty($report)): ?>
+            <?php if (empty($report['data'])): ?>
                 <tr>
                     <td class="px-4 py-4 text-center text-blue-400">No report data available.</td>
                 </tr>
             <?php else: ?>
-                <?php foreach ($report as $key => $value): ?>
+                <tr>
+                    <?php foreach ($report['columns'] as $col => $label): ?>
+                        <th class="px-4 py-2 text-left text-xs font-bold text-blue-800 uppercase tracking-wide border-b-2 border-blue-200">
+                            <?= htmlspecialchars($label) ?>
+                        </th>
+                    <?php endforeach; ?>
+                </tr>
+                <?php foreach ($report['data'] as $row): ?>
                     <tr>
-                        <td class="px-4 py-2"><?= htmlspecialchars($key . ': ' . $value) ?></td>
+                        <?php foreach ($report['columns'] as $col => $label): ?>
+                            <td class="px-4 py-2 whitespace-nowrap">
+                                <?= htmlspecialchars($row[$col] ?? '-') ?>
+                            </td>
+                        <?php endforeach; ?>
                     </tr>
                 <?php endforeach; ?>
             <?php endif; ?>
