3af9513620b944ba6d4fe1293a32817769b31717
Mohamed Imad <email2emad@gmail.com>
2025-07-26 07:27:53 +0500

    Added ReportBuilder

--- App/Views/admin/transactions/index.view.php
+++ App/Views/admin/transactions/index.view.php
@@ -23,7 +23,7 @@
                 </tr>
             <?php else: ?>
                 <?php foreach ($transactions as $transaction): ?>
-                    <tr class="<?= ($transaction->type ?? '') === 'income' ? 'bg-green-100' : 'bg-red-100'; ?> border-t border-blue-100 hover:bg-blue-50 transition">
+                    <tr class="<?= ($transaction->type ?? '') === 'income' ? 'text-green-600' : 'text-red-600'; ?> border-t border-blue-100 hover:bg-blue-50 transition">
                         <td class="px-4 py-2 whitespace-nowrap"><?= htmlspecialchars($transaction->date ?? '-') ?></td>
                         <td class="px-4 py-2 whitespace-nowrap"><?= htmlspecialchars($transaction->amount ?? '-') ?></td>
                         <td class="px-4 py-2 whitespace-nowrap"><?= htmlspecialchars($transaction->category_name ?? '-') ?></td>
