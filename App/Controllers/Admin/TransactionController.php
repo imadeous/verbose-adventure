eb986194c77d05cc3fe6e278ecb07856e7a67432
Mohamed Imad <email2emad@gmail.com>
2025-07-26 08:24:48 +0500

    Added ReportBuilder

--- App/Controllers/Admin/TransactionController.php
+++ App/Controllers/Admin/TransactionController.php
@@ -22,6 +22,7 @@ class TransactionController extends AdminControllerBase
         $dailyReport = ReportBuilder::build('transactions', 'date')
             ->forPeriod(date('Y-m-01'), date('Y-m-t')) // Aggregate for current month
             ->daily()
+            ->where('type', '=', 'expense') // Only include expenses
             // ->withEmptyNodes(true)
             ->withSum('amount', 'Total')
             ->withMax('amount', 'Max')
