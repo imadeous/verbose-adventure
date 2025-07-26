980681fc66c66e9ae6c3d6747660e777f6f653e1
Mohamed Imad <email2emad@gmail.com>
2025-07-26 08:23:14 +0500

    Added ReportBuilder

--- App/Views/partials/admin/sidebar.php
+++ App/Views/partials/admin/sidebar.php
@@ -57,6 +57,12 @@
                         </svg>
                         <a href="<?= url('admin/transactions') ?>" class="w-full">Transactions</a>
                     </li>
+                    <li class="<?= strpos($currentUrl, '/admin/reports') === 0 ? 'text-gray-200 bg-gray-800' : 'hover:text-white' ?> rounded px-4 py-3 flex space-x-4 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                        </svg>
+                        <a href="<?= url('admin/reports') ?>" class="w-full">Reports</a>
+                    </li>
                     <li class="<?= strpos($currentUrl, '/admin/reviews') === 0 ? 'text-gray-200 bg-gray-800' : 'hover:text-white' ?> rounded px-4 py-3 flex space-x-4 items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
