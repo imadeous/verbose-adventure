76e12e1d989d13049bd3b5309333e20be1e33092
Mohamed Imad <email2emad@gmail.com>
2025-07-27 14:41:48 +0500

    Added ReportBuilder

--- App/Views/admin/index.view.php
+++ App/Views/admin/index.view.php
@@ -179,7 +179,7 @@
                         <div class="bg-blue-100 border border-blue-200 rounded-lg p-4 flex flex-col gap-2 shadow-sm">
                             <div class="flex items-center gap-2">
                                 <span class="font-semibold text-blue-900"><?= e($review->customer_name) ?></span>
-                                <span class="text-xs text-blue-400 ml-auto flex space-x-1"><?= e($review->recommendation_score ?? '-') ?>
+                                <span class="text-xs text-blue-400 ml-auto flex space-x-2"><?= e($review->recommendation_score ?? '-') ?>
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
