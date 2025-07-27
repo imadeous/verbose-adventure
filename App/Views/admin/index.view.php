b79dca0e7ff4f23270b9e03a0fa83aeb9bafab0b
Mohamed Imad <email2emad@gmail.com>
2025-07-27 13:56:33 +0500

    Added ReportBuilder

--- App/Views/admin/index.view.php
+++ App/Views/admin/index.view.php
@@ -298,7 +298,7 @@
                             <span class="inline-block w-3 h-3 rounded-full text-blue-700"></span>
                             <span class="font-medium text-blue-900 flex-1"><?php echo Product::find($product['product_id'])->name ?? 'Unknown Product'; ?></span>
                             <div class="flex items-center gap-1 bg-blue-200 px-2 py-1 rounded-full">
-                                <span class="text-blue-700 font-bold"><?php echo number_format($product['rating'], 1); ?></span>
+                                <span class="text-blue-700 font-bold"><?php echo number_format($product['Overall Rating'], 1); ?></span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 text-blue-700">
                                     <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
                                 </svg>
