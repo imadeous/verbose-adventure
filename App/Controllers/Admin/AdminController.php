9a0d9561002b27d64c42ee104151cbd2cb46ea23
Mohamed Imad <email2emad@gmail.com>
2025-07-27 10:37:43 +0500

    Added ReportBuilder

--- App/Controllers/Admin/AdminController.php
+++ App/Controllers/Admin/AdminController.php
@@ -27,7 +27,7 @@ class AdminController extends AdminControllerBase
             ->withCount('*', 'Total Reviews')
             ->generate('Reviews Report', true);
 
-        $ratings = [
+        $matrices = [
             'Quality' => $reviewsReport['data'][0]['Quality'] ?? 0,
             'Pricing' => $reviewsReport['data'][0]['Pricing'] ?? 0,
             'Communication' => $reviewsReport['data'][0]['Communication'] ?? 0,
@@ -37,10 +37,10 @@ class AdminController extends AdminControllerBase
 
         $recommendPercent = $reviewsReport['data'][0]['Recommendation'] ?? 0;
 
-        $overallAvg = array_sum(array_values($ratings)) / count($ratings);
+        $overallAvg = array_sum(array_values($matrices)) / count($matrices);
 
         $ratingStats = [
-            'matrices' => $ratings,
+            'matrices' => $matrices,
             'recommendPercent' => $recommendPercent,
             'overallAvg' => $overallAvg,
             'count' => $reviewsReport['data'][0]['Total Reviews'] ?? 0,
