8d5b2a93d7938970f82f98dafbdf5cca0606e324
Mohamed Imad <email2emad@gmail.com>
2025-07-27 16:41:42 +0500

    Added ChartBuilder

--- App/Views/admin/index.view.php
+++ App/Views/admin/index.view.php
@@ -40,16 +40,9 @@ use App\Models\Product;
             $trendText = ($percent >= 0 ? '+' : '') . number_format($percent, 1) . '%';
             ?>
             <div class="bg-blue-200 <?= $percent >= 0 ? 'text-blue-600' : 'text-red-600' ?> rounded-lg p-3 flex items-center justify-center">
-                <?php if ($percent >= 0): ?>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
-                    </svg>
-                <?php endif; ?>
-                <?php if ($percent < 0): ?>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181" />
-                    </svg>
-                <?php endif; ?>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
+                </svg>
             </div>
             <div>
                 <div class="text-xs text-blue-400 font-medium uppercase tracking-wide">Trend</div>
