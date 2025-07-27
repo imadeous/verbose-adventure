b6f479f7c92686dbf7885ee5764f654e3d1b3dd6
Mohamed Imad <email2emad@gmail.com>
2025-07-27 10:10:31 +0500

    Added ReportBuilder

--- Core/Database/ReportBuilder.php
+++ Core/Database/ReportBuilder.php
@@ -294,7 +294,7 @@ class ReportBuilder extends QueryBuilder
     {
         $alias = $alias ?? "percent_{$column}";
         // (SUM(column) / COUNT(column) * base) * 100
-        $expr = "((SUM(`$column`) / NULLIF(COUNT(`$column`), 0)) * $base) * 100";
+        $expr = "((SUM(`$column`) / NULLIF(COUNT(`$column`), 0)) / $base) * 100";
         $this->aggregates[] = "$expr AS `$alias`";
         $this->columnAliases[$alias] = ucwords(str_replace('_', ' ', $alias));
         return $this;
