5a7ba214908f9dc5e0c48b4289a655520f9e30ae
Mohamed Imad <email2emad@gmail.com>
2025-07-27 14:25:28 +0500

    Added ReportBuilder

--- App/Views/layouts/admin.php
+++ App/Views/layouts/admin.php
@@ -23,7 +23,7 @@
             <!-- add breadcrumbs support here   -->
             <div class="flex flex-col flex-1 overflow-y-auto">
                 <?php echo $this->partial('_flash.view'); ?>
-                <?php include __DIR__ . '/../../partials/breadcrumb.view.php'; ?>
+                <?php echo $this->partial('breadcrumb.view'); ?>
                 <div class="p-4 sm:p-6 lg:p-12 space-y-8 mx-2 sm:mx-6 flex-1">
                     <?php echo $this->yield('content'); ?>
                 </div>
