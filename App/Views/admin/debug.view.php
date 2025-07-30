b86a30d4feda9a91abc03ec54920c3e1bf6f132a
Mohamed Imad <email2emad@gmail.com>
2025-07-30 19:55:34 +0500

    Added ChartBuilder

--- App/Views/admin/debug.view.php
+++ App/Views/admin/debug.view.php
@@ -13,7 +13,7 @@ else echo 'No SQL query set.'; ?>
             <h2 class="text-xl font-semibold mb-4 text-yellow-400">Variables Dump</h2>
             <pre class="bg-zinc-900 text-yellow-300 p-4 rounded-md whitespace-pre-wrap break-words text-base">
 <?php
-var_dump($vars['report']['data'] ?? 'No report data available.');
+var_dump($vars['report']['data'][0] ?? 'No report data available.');
 // echo mysql_real_escape_string('%DHC-6 300%'); // Removed: function not available in modern PHP
 ?>
             </pre>
