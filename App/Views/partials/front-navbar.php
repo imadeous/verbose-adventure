3a930ef5456d04c7443b74f453c943ac21eb04ed
Mohamed Imad <email2emad@gmail.com>
2025-11-10 18:05:19 +0500

    multi-select

--- App/Views/partials/front-navbar.php
+++ App/Views/partials/front-navbar.php
@@ -13,6 +13,7 @@
             <span class="ml-3 text-xl">Craftophile Shop</span>
         </a>
         <nav class="md:ml-auto md:mr-auto hidden md:flex flex-wrap items-center text-base justify-center">
+            <a href="<?= url('/') ?>" :class="isActive('<?= parse_url(url('/'), PHP_URL_PATH) ?>') ? 'text-yellow-500 mr-5 border-b-2 border-yellow-500' : 'mr-5 hover:text-white'">Home</a>
             <a href="<?= url('/gallery') ?>" :class="isActive('<?= parse_url(url('/gallery'), PHP_URL_PATH) ?>') ? 'text-yellow-500 mr-5 border-b-2 border-yellow-500' : 'mr-5 hover:text-white'">Gallery</a>
             <a href="<?= url('/quote') ?>" :class="isActive('<?= parse_url(url('/quote'), PHP_URL_PATH) ?>') ? 'text-yellow-500 mr-5 border-b-2 border-yellow-500' : 'mr-5 hover:text-white'">Get Quote</a>
             <a href="<?= url('/about') ?>" :class="isActive('<?= parse_url(url('/about'), PHP_URL_PATH) ?>') ? 'text-yellow-500 mr-5 border-b-2 border-yellow-500' : 'mr-5 hover:text-white'">About Us</a>
@@ -44,6 +45,14 @@
         return this.current === path.replace(/\/$/, '');
     }
 }" class="fixed bottom-0 left-0 right-0 z-50 bg-gray-900 border-t border-gray-800 flex justify-around items-center py-2 md:hidden">
+    <a href="<?= url('/') ?>" class="flex flex-col items-center text-xs" :class="isActive('<?= parse_url(url('/'), PHP_URL_PATH) ?>') ? 'text-yellow-500' : 'text-gray-400'">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
+        </svg>
+        Home
+    </a>
     <a href="<?= url('/gallery') ?>" class="flex flex-col items-center text-xs" :class="isActive('<?= parse_url(url('/gallery'), PHP_URL_PATH) ?>') ? 'text-yellow-500' : 'text-gray-400'">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="size-6">
