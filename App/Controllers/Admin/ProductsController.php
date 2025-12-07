e2e358b1594811dc11c9c42ed92180b527499095
Mohamed Imad <email2emad@gmail.com>
2025-12-07 17:41:10 +0500

    Fix sample transactions - use NULL for variant_id to avoid foreign key errors

--- App/Controllers/Admin/ProductsController.php
+++ App/Controllers/Admin/ProductsController.php
@@ -61,8 +61,8 @@ class ProductsController extends AdminControllerBase
             // ->withAverage('amount', 'Average')
             ->mixedChart([
                 'Revenue' => ['type' => 'line', 'yAxisID' => 'y1', 'borderColor' => '#2563eb', 'fill' => false],
-                // 'Orders' => ['type' => 'line', 'yAxisID' => 'y', 'backgroundColor' => '#60a5fa'],
-                'Average' => ['type' => 'line', 'yAxisID' => 'y', 'borderColor' => '#ffa5fa', 'fill' => false]
+                'Orders' => ['type' => 'bar', 'yAxisID' => 'y', 'backgroundColor' => '#ffa5fa'],
+                'Average' => ['type' => 'line', 'yAxisID' => 'y1', 'borderColor' => '#ffa5fa', 'fill' => false]
             ])
             ->toArray();
 
