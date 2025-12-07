6ac093c3ee04dd017bc51366355e8d43accc309b
Mohamed Imad <email2emad@gmail.com>
2025-12-07 16:20:35 +0500

    Fix Variant model fillable - use weight_grams instead of weight

--- App/Models/Variant.php
+++ App/Models/Variant.php
@@ -11,7 +11,7 @@ class Variant extends Model
     protected $fillable = [
         'product_id',
         'dimensions',
-        'weight',
+        'weight_grams',
         'material',
         'color',
         'finishing',
