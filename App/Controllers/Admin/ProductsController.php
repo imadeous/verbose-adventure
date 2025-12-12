9fe2b41bb0ab8d5649567094febd37f37e0871fa
Mohamed Imad <email2emad@gmail.com>
2025-12-12 18:00:03 +0500

    Implemented AI business Assistant

--- App/Controllers/Admin/ProductsController.php
+++ App/Controllers/Admin/ProductsController.php
@@ -41,7 +41,8 @@ class ProductsController extends AdminControllerBase
 
             // Calculate stock value
             $product->stock_value = Product::getStockValue($product->id);
-
+            var_dump($product->stock_value);
+            exit;
             $enrichedProducts[] = $product;
         }
 
