7acc25c3d81214cbc8d8f56cdde7c3717ff20a71
Mohamed Imad <email2emad@gmail.com>
2025-12-23 18:23:57 +0500

    Push Calendar

--- App/Views/admin/transactions/index.view.php
+++ App/Views/admin/transactions/index.view.php
@@ -64,15 +64,23 @@
                                 }
                                 ?>
                             </td>
-                            <td class="px-4 py-2 whitespace-nowrap hidden lg:table-cell">
-                                <?php if (!empty($transaction->quote_id)): ?>
-                                    <a href="<?= url('admin/quotes/' . $transaction->quote_id) ?>" class="text-blue-600 hover:underline">#<?= htmlspecialchars($transaction->quote_id) ?></a>
-                                <?php else: ?>
-                                    -
-                                <?php endif; ?>
-                            </td>
-                            <td class="px-4 py-2 whitespace-nowrap hidden">
-                                <a href="<?= url('admin/transactions/show/' . $transaction->id) ?>" class="text-blue-600 hover:underline">View</a>
+                            <td class="px-4 py-2 whitespace-nowrap">
+                                <div class="flex gap-2">
+                                    <a href="<?= url('admin/transactions/' . $transaction->id . '/edit') ?>" class="text-blue-600 hover:text-blue-800">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                                        </svg>
+                                    </a>
+                                    <form action="<?= url('admin/transactions/' . $transaction->id) ?>" method="POST" onsubmit="return confirm('Are you sure you want to delete this transaction?');" class="inline">
+                                        <?= csrf_field() ?>
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <button type="submit" class="text-red-600 hover:text-red-800">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
                     <?php endforeach; ?>
