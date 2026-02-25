84e0211ad83c63bb9e26ae22604ca060fb85550f
Mohamed Imad <email2emad@gmail.com>
2026-02-25 23:42:10 +0500

    Storefront overhaul

--- public/index.php
+++ public/index.php
@@ -5,9 +5,9 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 // Enable error reporting for debugging
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+// error_reporting(E_ALL);
 
 // The following duplicate PHP opening tag and session_start block have been removed.
 use Core\Router;
