7e050bc672e2903471715195fcf82386c26a15e4
Mohamed Imad <email2emad@gmail.com>
2026-02-28 02:30:33 +0500

    Storefront overhaul

--- App/Views/buy.view.php
+++ App/Views/buy.view.php
@@ -101,7 +101,7 @@ $Gallery = Gallery::select(['id', 'related_id', 'image_url'])
                 <div class="p-2 w-full">
                     <div class="relative">
                         <label for="address" class="leading-7 text-sm text-gray-400">Address</label>
-                        <input type="text" id="address" name="address" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-yellow-500 focus:bg-gray-900 focus:ring-2 focus:ring-yellow-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                        <textarea id="address" name="address" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-yellow-500 focus:bg-gray-900 focus:ring-2 focus:ring-yellow-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                     </div>
                 </div>
                 <div class="p-2 w-full">
