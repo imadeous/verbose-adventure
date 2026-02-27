ecd44b633765e38caf144f46bb72447668aface1
Mohamed Imad <email2emad@gmail.com>
2026-02-28 02:32:00 +0500

    Storefront overhaul

--- App/Views/buy.view.php
+++ App/Views/buy.view.php
@@ -111,6 +111,12 @@ $Gallery = Gallery::select(['id', 'related_id', 'image_url'])
                     </div>
                 </div>
                 <div class="p-2 w-full">
+                    <div class="relative">
+                        <label for="payment_slip" class="leading-7 text-sm text-gray-400">Payment Slip </label>
+                        <input type="file" id="payment_slip" name="payment_slip" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-yellow-500 focus:bg-gray-900 focus:ring-2 focus:ring-yellow-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                    </div>
+                </div>
+                <div class="p-2 w-full">
                     <button class="flex mx-auto text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
                 </div>
             </div>
