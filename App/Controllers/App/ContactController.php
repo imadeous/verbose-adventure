50f1d466ab9a190d7bcac1eada78eaedd9f0640d
Mohamed Imad <email2emad@gmail.com>
2026-02-27 23:00:32 +0500

    Storefront overhaul

--- App/Controllers/App/ContactController.php
+++ App/Controllers/App/ContactController.php
@@ -44,12 +44,6 @@ class ContactController extends Controller
         ]);
         $contact->save();
 
-        var_dump(
-            getenv('TELEGRAM_BOT_TOKEN'),
-            [getenv('TELEGRAM_CHAT_ID')]
-        );
-        die();
-
         // Send Telegram notification
         try {
 
